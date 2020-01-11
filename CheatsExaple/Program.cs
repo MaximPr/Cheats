52c0c5a8baf3e14f2b85671991d44b35e238212c
MaximPr <maxim.pr@mail.ru>
2020-01-11 19:13:50 +0300

    библиотека и пример

---
+++ CheatsExaple/Program.cs
@@ -0,0 +1,42 @@
+﻿using CheatsLib;
+using System;
+
+namespace CheatsExaple
+{
+    class Program
+    {
+        public class Player
+        {
+            public string Name;
+        }
+
+        static bool quit = false;
+        static void Main(string[] args)
+        {
+            var cheats = InitCheats();
+
+            Player p = new Player { Name = "Alex" };
+
+            Console.WriteLine("Enter cheat!");
+
+            while (!quit)
+            {
+                Console.Write(">>");
+                string message = Console.ReadLine();
+                cheats.TryRunCheat(message, p);
+            }
+        }
+
+        static Cheats<Player> InitCheats()
+        {
+            Cheats<Player> cheats = new Cheats<Player>();
+            cheats.RegisterCheat("help", (p) => Console.WriteLine(cheats.GetCheatDescription()), "Print all cheat descriptions");
+            cheats.RegisterCheat("getname", (p) => Console.WriteLine("Name: " + p.Name), "Print my name");
+            cheats.RegisterCheat<string>("setname", (p, name) => p.Name = name, "Set my name");
+            cheats.RegisterCheat("quit", (p) => quit = true, "Quit");
+            cheats.RegisterCheat("q", (p) => quit = true, "Quit");
+
+            return cheats;
+        }
+    }
+}
---
+++ CheatsLib/Cheats.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CheatsLib
+{
+    public class Cheats<T>
+    {
+        public Regex PatternCheat = new Regex("/( )?(?<message>.+)");
+        private Regex argsParser = new Regex(@"(?<name>\w+)(?<args> .*)?");
+        private char[] separators = new char[] { ' ' };
+        private Dictionary<string, Action<T, string[]>> cheatDic = new Dictionary<string, Action<T, string[]>>();
+        private List<KeyValuePair<string, string>> cheatsDescr = new List<KeyValuePair<string, string>>();
+
+        public Cheats()
+        {
+        }
+
+        public void Clear()
+        {
+            cheatDic.Clear();
+            cheatsDescr.Clear();
+        }
+
+        public bool TryRunCheat(string InputMessage, T person)
+        {
+            var m = PatternCheat.Match(InputMessage);
+            if (m.Success)
+            {
+                string CheatmMessage = m.Groups["message"].Value;
+                var m2 = argsParser.Match(CheatmMessage);
+                var name = m2.Groups["name"].Value;
+                name = name.ToLower();
+                return RunCheat(CheatmMessage, person);
+            }
+            return false;
+        }
+
+        public bool RunCheat(string message, T p)
+        {
+            var m = argsParser.Match(message);
+            var name = m.Groups["name"].Value;
+            name = name.ToLower();
+            var argsStr = m.Groups["args"].Value;
+            string[] args = argsStr.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            Action<T, string[]> action;
+            if (cheatDic.TryGetValue(args.Length + name, out action))
+            {
+                action(p, args);
+                return true;
+            }
+
+            return false;
+        }
+
+        public string GetCheatDescription()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var pair in cheatsDescr)
+            {
+                sb.Append(pair.Key);
+                if (!string.IsNullOrEmpty(pair.Value))
+                    sb.AppendLine(" - " + pair.Value);
+            }
+
+            return sb.ToString();
+        }
+
+        private void Register(string name, int argsCount, Action<T, string[]> func, string title, string description)
+        {
+            name = name.ToLower();
+            cheatDic.Add(argsCount + name, func);
+            cheatsDescr.Add(new KeyValuePair<string, string>(title, description));
+
+            cheatsDescr.Sort((x, y) => string.Compare(x.Key, y.Key));
+        }
+
+        public void RegisterCheat(string name, Action<T> f, string descr = "")
+        {
+            Register(name, 0, (p, x) => f(p), "/" + name, descr);
+        }
+
+        public void RegisterCheat(string name, Action<T, string> f, string descr = "")
+        {
+            Register(name, 1, (p, x) => f(p, x[0]), "/" + name + " string", descr);
+        }
+
+        public void RegisterCheat(string name, Action<object, int> f, string descr = "")
+        {
+            Register(name, 1, (p, x) => f(p, int.Parse(x[0])), "/" + name + " int", descr);
+        }
+
+        public void RegisterCheat(string name, Action<object, string, bool> f, string descr = "")
+        {
+            Register(name, 2, (p, x) => f(p, x[0], bool.Parse(x[1])), "/" + name + " string bool", descr);
+        }
+
+        public void RegisterCheat(string name, Action<object, int, int> f, string descr = "")
+        {
+            Register(name, 2, (p, x) => f(p, int.Parse(x[0]), int.Parse(x[1])), "/" + name + " int int", descr);
+        }
+
+        public void RegisterCheat(string name, Action<object, int, string> f, string descr = "")
+        {
+            Register(name, 2, (p, x) => f(p, int.Parse(x[0]), x[1]), "/" + name + " int string", descr);
+        }
+
+        public void RegisterCheat(string name, Action<object, string, int> f, string descr = "")
+        {
+            Register(name, 2, (p, x) => f(p, x[0], int.Parse(x[1])), "/" + name + " string int", descr);
+        }
+
+        public void RegisterCheat(string name, Action<object, string, string> f, string descr = "")
+        {
+            Register(name, 2, (p, x) => f(p, x[0], x[1]), "/" + name + " string string", descr);
+        }
+
+        public void RegisterCheat(string name, Action<object, string, string, string> f, string descr = "")
+        {
+            Register(name, 3, (p, x) => f(p, x[0], x[1], x[2]), "/" + name + " string string string", descr);
+        }
+
+        public void RegisterCheat(string name, Action<object, int, int, int> f, string descr = "")
+        {
+            Register(name, 3, (p, x) => f(p, int.Parse(x[0]), int.Parse(x[1]), int.Parse(x[2])), "/" + name + " int int int", descr);
+        }
+
+        public void RegisterCheat<T1>(string name, Action<T, T1> f, string descr = "")
+        {
+            var args = f.GetType().GetGenericArguments().ToList();
+            args.RemoveAt(0);
+            Register(name, args.Count,
+                (p, x) => f(p,
+                    (T1)Convert.ChangeType(x[0], typeof(T1), CultureInfo.InvariantCulture)
+                ),
+                "/" + name + " " + string.Join(" ", args.Select(x => x.Name).ToArray()),
+                descr
+            );
+        }
+
+        public void RegisterCheat<T1, T2>(string name, Action<T, T1, T2> f, string descr = "")
+        {
+            var args = f.GetType().GetGenericArguments().ToList();
+            args.RemoveAt(0);
+            Register(name, args.Count,
+                (p, x) => f(p,
+                    (T1)Convert.ChangeType(x[0], typeof(T1), CultureInfo.InvariantCulture),
+                    (T2)Convert.ChangeType(x[1], typeof(T2), CultureInfo.InvariantCulture)
+                ),
+                "/" + name + " " + string.Join(" ", args.Select(x => x.Name).ToArray()),
+                descr
+            );
+        }
+
+        public void RegisterCheat<T1, T2, T3>(string name, Action<T, T1, T2, T3> f, string descr = "")
+        {
+            var args = f.GetType().GetGenericArguments().ToList();
+            args.RemoveAt(0);
+            Register(name, args.Count,
+                (p, x) => f(p,
+                    (T1)Convert.ChangeType(x[0], typeof(T1), CultureInfo.InvariantCulture),
+                    (T2)Convert.ChangeType(x[1], typeof(T2), CultureInfo.InvariantCulture),
+                    (T3)Convert.ChangeType(x[2], typeof(T3), CultureInfo.InvariantCulture)
+                ),
+                "/" + name + " " + string.Join(" ", args.Select(x => x.Name).ToArray()),
+                descr
+            );
+        }
+
+        public void RegisterCheat<T1, T2, T3, T4>(string name, Action<T, T1, T2, T3, T4> f, string descr = "")
+        {
+            var args = f.GetType().GetGenericArguments().ToList();
+            args.RemoveAt(0);
+            Register(name, args.Count,
+                (p, x) => f(p,
+                    (T1)Convert.ChangeType(x[0], typeof(T1), CultureInfo.InvariantCulture),
+                    (T2)Convert.ChangeType(x[1], typeof(T2), CultureInfo.InvariantCulture),
+                    (T3)Convert.ChangeType(x[2], typeof(T3), CultureInfo.InvariantCulture),
+                    (T4)Convert.ChangeType(x[3], typeof(T4), CultureInfo.InvariantCulture)
+                ),
+                "/" + name + " " + string.Join(" ", args.Select(x => x.Name).ToArray()),
+                descr
+            );
+        }
+    }
+}
+
